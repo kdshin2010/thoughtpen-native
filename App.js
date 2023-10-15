ccdca3283e436f9be346bb6aff6fb8182f2dda4d
Kyle Shin <kdshin2010@gmail.com>
2023-10-14 18:27:06 -0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
+import { Button } from 'react-native-paper';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Open up App.js to start working on yEEEour eapp!</Text>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+    </PaperProvider>
   );
 }
 
